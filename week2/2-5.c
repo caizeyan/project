3636ab4bf8e7cf43c54bc2d08a3ae555f820cc1a
caizeyan <772373218@qq.com>
2020-09-28 04:39:19 -0700

    week2

---
+++ week2/2-5.c
@@ -0,0 +1,13 @@
+#include"2-2.h"
+
+int main(void){
+	int cnt = 0;
+	while(1){
+		char name[64];
+		snprintf(name,sizeof(name),"%d,txt",cnt);
+		int fd = creat(name,0644);	
+		cnt++;
+	}
+	return 0;
+
+}
