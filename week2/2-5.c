bb78706fa2f4a5ee7b1946a9ec31dfeb9305cd6f
caizeyan <772373218@qq.com>
2020-09-29 08:04:57 -0700

    update

--- week2/2-5.c
+++ week2/2-5.c
@@ -1,13 +1,14 @@
-#include"2-2.h"
+	#include"2-2.h"
 
-int main(void){
-	int cnt = 0;
-	while(1){
-		char name[64];
-		snprintf(name,sizeof(name),"%d,txt",cnt);
-		int fd = creat(name,0644);	
-		cnt++;
+	int main(void){
+		int cnt = 0;
+		while(1){
+			char name[64];
+			snprintf(name,sizeof(name),"%d,txt",cnt);
+			int fd = creat(name,0644);	
+			if(fd == -1) break;
+			cnt++;
+		}
+		return 0;
+
 	}
-	return 0;
-
-}
